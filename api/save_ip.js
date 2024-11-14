ee720edae88bf7c692a798a24cf41d8a4fa9f143
regii17 <reegii90@gmail.com>
2024-11-15 03:38:51 +0700

    UPP

--- api/save_ip.js
+++ api/save_ip.js
@@ -2,19 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 export default function handler(req, res) {
-    // Dapatkan alamat IP pengguna
     const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    // Tentukan path file tempat IP akan disimpan
-    const filePath = path.join(process.cwd(), 'ip.txt');
-
-    // Tulis IP ke file dengan cara append (menambahkan di akhir file)
-    fs.appendFile(filePath, `${userIp}\n`, (err) => {
-        if (err) {
-            console.error('Error saving IP:', err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        res.status(200).send('IP Anda telah tercatat.');
-    });
+    console.log(`User IP: ${userIp}`);
+    res.status(200).send('IP Anda telah tercatat di log.');
 }
+
