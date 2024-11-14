01138c176ed6346a17fa6e96ee99705e00a12a49
regii17 <reegii90@gmail.com>
2024-11-15 04:06:09 +0700

    u

--- api/save_ip.js
+++ api/save_ip.js
@@ -4,6 +4,7 @@ const path = require('path');
 export default function handler(req, res) {
     const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`User IP: ${userIp}`);
-    res.status(200).send('IP Anda telah tercatat di log.');
-}
 
+    res.writeHead(302, { Location: 'https://drive.google.com/drive/folders/1m2Fs1OaE2wfJWTMCu-M5-f4ltnbQQN1L' });
+    res.end();
+}
